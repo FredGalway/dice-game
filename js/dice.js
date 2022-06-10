f0237ed8d2f7c7e551442d94b34161d2f94a1c30
FredGalway <fmoitry@gmail.com>
2022-06-10 16:07:52 +0200

    Score ROUND, Score GLOBAL OK

--- js/dice.js
+++ js/dice.js
@@ -1,56 +1,52 @@
 // VARIABLES DU JEU DE DÉ
 
-// Colonne centrale
+// Attribution des éléments HTML 
 let new_game = document.querySelector('#new_game');
 let cube = document.querySelector('.cube');
 let roll_dice = document.querySelector('.roll_dice');
 let hold = document.querySelector('.hold');
 
-// Colonnes Players
-let current_player = '';
-let currentClass = '';
-let GLOBAL_player1 = 0;
-let GLOBAL_player2 = 0;
-let ROUND_player1 = 0;
-let ROUND_player2 = 0;
-let result = "Your turn";
-
 // Nombre aléatoire et attribution dynamique de class
 let randNum;
 let showClass;
+let currentClass = '';
+let current_player = 'p1';
+let ROUND_player1_tmp = 0;
+let ROUND_player2_tmp = 0;
+let ROUND_player1 = 0;
+let ROUND_player2 = 0;
+let GLOBAL_player1 = 0;
+let GLOBAL_player2 = 0;
+let result = "Player 1's turn";
 
 // Fonction lancement des paramètres du jeu
 function initialize() {
-    GLOBAL_player1 = 0;
-    GLOBAL_player2 = 0;
-    ROUND_player1 = 0;
-    ROUND_player2 = 0;
-    current_player = 'p1';
     console.log("Nouvelle Partie / Player 1 " + current_player);
+    // Remplacement de innerHTML par textcontent 
+    document.getElementById("GLOBAL_player1").textContent = GLOBAL_player1;
+    document.getElementById("GLOBAL_player2").textContent = GLOBAL_player2;
+    document.getElementById("ROUND_player1").textContent = ROUND_player1_tmp;
+    document.getElementById("ROUND_player2").textContent = ROUND_player2_tmp;
 
-    document.getElementById("GLOBAL_player1").innerHTML = GLOBAL_player1;
-    document.getElementById("GLOBAL_player2").innerHTML = GLOBAL_player2;
-    document.getElementById("ROUND_player1").innerHTML = ROUND_player1;
-    document.getElementById("ROUND_player2").innerHTML = ROUND_player2;
+    console.clear();
 }
-
+// Lancement des paramètres du jeu
 initialize();
 
-
-// Fonction lancement du Jeu 
+// Fonction Nouvelle Partie
 function newGame() {
     cube.classList.remove(currentClass);
     cube.classList.add("showClass-1");
     initialize();
 }
-// Fonction de nombres aléatoires 
+// Fonction des nombres aléatoires 
 function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
 }
-
-function dynamic_faces_and_classes() {
+// Attribution dynamique des faces du Dé
+function dynamic_dice_faces() {
     // Attribution d'un nombre entre 1 et 7
     randNum = getRandomInt(1, 7)
     showClass = 'show-' + randNum;
@@ -62,72 +58,62 @@ function dynamic_faces_and_classes() {
     cube.classList.add(showClass);
     currentClass = showClass;
 }
+
 // Fonction du Jeu de Dé en 3D
 
 // PLAYER 1
 function player1() {
 
-    dynamic_faces_and_classes();
+    dynamic_dice_faces();
 
     // CONDITIONS DU JEU
 
     // Cas Dé face 1
     if (showClass == "show-1") {
-        ROUND_player1 = 0;
-        console.log("Player 1 / ROUND Score = " + ROUND_player1);
-        result = "Game Lost!";
-        document.getElementById("ROUND_player1").innerHTML = ROUND_player1;
-        //console.log("Perdu!");
+        ROUND_player1_tmp = 0;
+        console.log("Player 1 / ROUND Score = " + ROUND_player1_tmp);
+        result = "Player 1's turn lost!";
+        document.getElementById("ROUND_player1").textContent = ROUND_player1_tmp;
     }
 
-    // Cas Dé autres face
+    // Cas Dé autres faces
     if (showClass != "show-1") {
+        ROUND_player1_tmp = ROUND_player1_tmp + randNum;
         ROUND_player1 = ROUND_player1 + randNum;
-        console.log("Player 1 / ROUND Score = " + ROUND_player1);
+        console.log("Player 1 / ROUND Score = " + ROUND_player1_tmp);
         result = "Your turn";
-        document.getElementById("ROUND_player1").innerHTML = ROUND_player1;
+        document.getElementById("ROUND_player1").textContent = ROUND_player1_tmp;
     }
 
-    // Maximum GLOBAL Score Player1
-    if (GLOBAL_player1 >= 30) {
-        result = "Game Won!"
-            //console.log("Gagné!");
-    }
-
-    document.getElementById("result").innerHTML = result;
+    document.getElementById("result").textContent = result;
 }
 
 // PLAYER 2
 function player2() {
 
-    dynamic_faces_and_classes();
+    dynamic_dice_faces();
 
     // CONDITIONS DU JEU
 
     // Cas Dé face 1
     if (showClass == "show-1") {
-        ROUND_player2 = 0;
-        console.log("Player 2 / ROUND Score = " + ROUND_player2);
+        ROUND_player2_tmp = 0;
+        console.log("Player 2 / ROUND Score = " + ROUND_player2_tmp);
         result = "Game Lost!";
-        document.getElementById("ROUND_player2").innerHTML = ROUND_player2;
+        document.getElementById("ROUND_player2").textContent = ROUND_player2_tmp;
         //console.log("Perdu!");
     }
 
-    // Cas Dé autres face
+    // Cas Dé autres faces
     if (showClass != "show-1") {
+        ROUND_player2_tmp = ROUND_player2_tmp + randNum;
         ROUND_player2 = ROUND_player2 + randNum;
-        console.log("Player 2 / ROUND Score = " + ROUND_player2);
+        console.log("Player 2 / ROUND Score = " + ROUND_player2_tmp);
         result = "Your turn";
-        document.getElementById("ROUND_player2").innerHTML = ROUND_player2;
+        document.getElementById("ROUND_player2").textContent = ROUND_player2_tmp;
     }
 
-    // Maximum GLOBAL Score Player2
-    if (GLOBAL_player2 >= 30) {
-        result = "Game Won!"
-            //console.log("Gagné!");
-    }
-
-    document.getElementById("result").innerHTML = result;
+    document.getElementById("result").textContent = result;
 }
 
 // Lancement des fonctions sur l'évènement click
@@ -138,17 +124,30 @@ hold.addEventListener("click", holdResult);
 // Bouton enregistrement du Score ROUND dans GLOBAL et permutation du Joueur 
 function holdResult() {
     if (current_player == 'p1') {
+
         GLOBAL_player1 = ROUND_player1;
-        document.getElementById("GLOBAL_player1").innerHTML = GLOBAL_player1;
+
+        document.getElementById("GLOBAL_player1").textContent = GLOBAL_player1;
         console.log("Player 1 / GLOBAL Score = " + GLOBAL_player1);
-        ROUND_player1 = 0;
-        document.getElementById("ROUND_player1").innerHTML = ROUND_player1;
+        ROUND_player1_tmp = 0;
+        document.getElementById("ROUND_player1").textContent = ROUND_player1_tmp;
+        // Maximum GLOBAL Score Player1
+        if (GLOBAL_player1 >= 40) {
+            result = "Player 1 Won!"
+            document.getElementById("result").textContent = result;
+        }
     } else if (current_player == 'p2') {
         GLOBAL_player2 = ROUND_player2;
-        document.getElementById("GLOBAL_player2").innerHTML = GLOBAL_player2;
+
+        document.getElementById("GLOBAL_player2").textContent = GLOBAL_player2;
         console.log("Player 2 / GLOBAL Score = " + GLOBAL_player2);
-        ROUND_player2 = 0;
-        document.getElementById("ROUND_player2").innerHTML = ROUND_player2;
+        ROUND_player2_tmp = 0;
+        document.getElementById("ROUND_player2").textContent = ROUND_player2_tmp;
+        // Maximum GLOBAL Score Player1
+        if (GLOBAL_player2 >= 40) {
+            result = "Player 2 Won!"
+            document.getElementById("result").textContent = result;
+        }
     }
     switchPlayer();
 }
