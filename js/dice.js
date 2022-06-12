0ba9b76f8b1e1aa2fd9552e8630e27ffe1f70141
FredGalway <fmoitry@gmail.com>
2022-06-12 11:11:36 +0200

    modification : variable status, condition else if, fonction witness

--- js/dice.js
+++ js/dice.js
@@ -1,5 +1,3 @@
-// VARIABLES DU JEU DE DÉ
-
 // Variables des Boutons HTML et du Cube Dé(dice)
 let new_game = document.querySelector('#new_game');
 let roll_dice = document.querySelector('.roll_dice');
@@ -10,11 +8,11 @@ let player1_title_witness_active = document.querySelector('#player1 .witness');
 let player1_title_witness_default = document.querySelector('#player1 .witness');
 let player2_title_witness_active = document.querySelector('#player2 .witness');
 let player2_title_witness_default = document.querySelector('#player2 .witness');
+player1_title_witness_active.style.opacity = 1;
+player2_title_witness_active.style.opacity = 0;
 
 // Variables du Jeu
-let randNum;
-let showClass;
-let currentClass;
+let randNum, showClass, currentClass, game_status;
 let current_player = 'p1';
 let ROUND_player1_tmp = 0;
 let ROUND_player2_tmp = 0;
@@ -23,16 +21,25 @@ let ROUND_player2 = 0;
 let GLOBAL_player1 = 0;
 let GLOBAL_player2 = 0;
 let win_score = 30;
-let result;
 let audio_turn_lost = new Audio("audio/lost.wav");
 audio_turn_lost.volume = 0.3;
 
+function witness() {
+    if (current_player != 'p1') {
+        player1_title_witness_active.style.opacity = 1;
+        player2_title_witness_active.style.opacity = 0;
+    } else {
+        player1_title_witness_active.style.opacity = 0;
+        player2_title_witness_active.style.opacity = 1;
+    }
+}
 
 // Fonction lancement des paramètres du jeu
 function initialize() {
+
+    current_player = 'p1';
     player1_title_witness_active.style.opacity = 1;
     player2_title_witness_active.style.opacity = 0;
-    current_player = 'p1';
     roll_dice.addEventListener("click", player1);
     roll_dice.removeEventListener("click", player2);
     ROUND_player1_tmp = 0;
@@ -41,7 +48,8 @@ function initialize() {
     ROUND_player2 = 0;
     GLOBAL_player1 = 0;
     GLOBAL_player2 = 0;
-    result = "Player 1's turn";
+    game_status = "Player 1's turn";
+
     // Remplacement de innerHTML par textcontent 
     document.getElementById("ROUND_player1").textContent = ROUND_player1_tmp;
     document.getElementById("ROUND_player2").textContent = ROUND_player2_tmp;
@@ -49,13 +57,15 @@ function initialize() {
     document.getElementById("ROUND_player2").textContent = ROUND_player2;
     document.getElementById("GLOBAL_player1").textContent = GLOBAL_player1;
     document.getElementById("GLOBAL_player2").textContent = GLOBAL_player2;
-    document.getElementById("result").textContent = result;
+    document.getElementById("game_status").textContent = game_status;
 
-    console.clear();
+    //console.clear();
     console.log("Nouvelle Partie / Player 1 ");
 }
 // Lancement des paramètres du jeu
 initialize();
+
+
 
 // Fonction Nouvelle Partie
 function newGame() {
@@ -98,7 +108,7 @@ function player1() {
         console.log("ROUND_player1_tmp = " + ROUND_player1_tmp);
         console.log("ROUND_player1 = " + ROUND_player1);
 
-        console.clear();
+        //console.clear();
         console.log("Changement de joueur");
 
         audio_turn_lost.play();
@@ -131,7 +141,7 @@ function player2() {
         console.log("ROUND_player2_tmp = " + ROUND_player2_tmp);
         console.log("ROUND_player2 = " + ROUND_player2);
 
-        console.clear();
+        //console.clear();
         console.log("Changement de joueur");
 
         audio_turn_lost.play();
@@ -139,7 +149,7 @@ function player2() {
     }
 
     // Cas Dé autres faces
-    else if (showClass != "show-1") {
+    else {
         ROUND_player2_tmp = ROUND_player2_tmp + randNum;
         ROUND_player2 = ROUND_player2 + randNum;
         console.log("Player 2 / ROUND Score tmp = " + ROUND_player2_tmp);
@@ -158,7 +168,7 @@ function holdResult() {
 
     if (current_player == 'p1') {
 
-        console.clear();
+        // console.clear();
         console.log("HOLD Score Player 1");
         GLOBAL_player1 = ROUND_player1;
         ROUND_player1_tmp = 0;
@@ -168,9 +178,9 @@ function holdResult() {
         console.log("Player 1 / ROUND tmp Score = " + ROUND_player1_tmp);
         console.log("Player 1 / ROUND Score = " + ROUND_player1);
 
-    } else if (current_player == 'p2') {
+    } else {
 
-        console.clear();
+        //console.clear();
         console.log("HOLD Score Player 2");
         GLOBAL_player2 = ROUND_player2;
         ROUND_player2_tmp = 0;
@@ -192,37 +202,36 @@ function switchPlayer() {
     ROUND_player1_tmp = 0;
     ROUND_player2_tmp = 0;
 
+    witness();
+
     if (current_player == 'p1') {
-        player1_title_witness_active.style.opacity = 1;
-        player2_title_witness_active.style.opacity = 0;
         roll_dice.addEventListener("click", player2);
         roll_dice.removeEventListener("click", player1);
         current_player = 'p2';
-        result = "Player 2's turn";
+        game_status = "Player 2's turn";
         console.log('Player2');
 
-    } else if (current_player == 'p2') {
-        player1_title_witness_active.style.opacity = 0;
-        player2_title_witness_active.style.opacity = 1;
+    } else {
         roll_dice.addEventListener("click", player1);
         roll_dice.removeEventListener("click", player2);
         current_player = 'p1';
-        result = "Player 1's turn";
+        game_status = "Player 1's turn";
         console.log('Player1');
     }
-    document.getElementById("result").textContent = result;
+    document.getElementById("game_status").textContent = game_status;
     winner();
+
 }
 
 function winner() {
     // Maximum GLOBAL Score Player1
     if (GLOBAL_player1 >= win_score) {
-        result = "Player 1 wins!"
-        document.getElementById("result").textContent = result;
+        game_status = "Player 1 wins!"
+        document.getElementById("game_status").textContent = game_status;
     }
     // Maximum GLOBAL Score Player1
     else if (GLOBAL_player2 >= win_score) {
-        result = "Player 2 wins!"
-        document.getElementById("result").textContent = result;
+        game_status = "Player 2 wins!"
+        document.getElementById("game_status").textContent = game_status;
     }
 }
