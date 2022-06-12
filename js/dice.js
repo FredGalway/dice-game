386006037e433ac0c59773e88eec3a4863651e4d
FredGalway <fmoitry@gmail.com>
2022-06-12 19:22:28 +0200

    le score fonctionnegit add *

--- js/dice.js
+++ js/dice.js
@@ -14,19 +14,16 @@ let players_background_active = document.querySelector('#active_bg');
 // Variables du Jeu
 let randNum, showClass, currentClass, game_status;
 let current_player = 'p1';
-let ROUND_player1_tmp = 0;
-let ROUND_player2_tmp = 0;
-let ROUND_player1 = 0;
-let ROUND_player2 = 0;
-let GLOBAL_player1 = 0;
-let GLOBAL_player2 = 0;
-let win_score = 30;
+let ROUND_player1;
+let ROUND_player2;
+let GLOBAL_player1;
+let GLOBAL_player2;
+let win_score = 100;
 let audio_turn_lost = new Audio("audio/lost.wav");
 audio_turn_lost.volume = 0.3;
 
 // Fonction lancement des paramètres du jeu
 function initialize() {
-
     current_player = 'p1';
     player1_title_witness_active.style.opacity = 1;
     player2_title_witness_active.style.opacity = 0;
@@ -35,8 +32,6 @@ function initialize() {
     players_background_active.style.right = 0 + 'px';
     roll_dice.addEventListener("click", player1);
     roll_dice.removeEventListener("click", player2);
-    ROUND_player1_tmp = 0;
-    ROUND_player2_tmp = 0;
     ROUND_player1 = 0;
     ROUND_player2 = 0;
     GLOBAL_player1 = 0;
@@ -44,16 +39,16 @@ function initialize() {
     game_status = "Player 1's turn";
 
     // Remplacement de innerHTML par textcontent 
-    document.getElementById("ROUND_player1").textContent = ROUND_player1_tmp;
-    document.getElementById("ROUND_player2").textContent = ROUND_player2_tmp;
     document.getElementById("ROUND_player1").textContent = ROUND_player1;
     document.getElementById("ROUND_player2").textContent = ROUND_player2;
     document.getElementById("GLOBAL_player1").textContent = GLOBAL_player1;
     document.getElementById("GLOBAL_player2").textContent = GLOBAL_player2;
     document.getElementById("game_status").textContent = game_status;
 
-    console.clear();
-    console.log("Nouvelle Partie / Player 1 ");
+    // Lancement des fonctions sur l'évènement click 
+    new_game.addEventListener("click", newGame);
+    roll_dice.addEventListener("click", player1)
+    hold.addEventListener("click", hold_fx);
 }
 // Lancement des paramètres du jeu
 initialize();
@@ -74,7 +69,6 @@ function active_player() {
         players_background_active.style.right = 490 + 'px';
     }
 }
-
 // Fonction Nouvelle Partie
 function newGame() {
     cube.classList.remove(currentClass);
@@ -92,7 +86,6 @@ function dynamic_dice_faces() {
     // Attribution d'un nombre entre 1 et 7
     randNum = getRandomInt(1, 7)
     showClass = 'show-' + randNum;
-
     // Modifications dynamique de class css sur l'élément du Dé 3D (cube)
     if (currentClass) {
         cube.classList.remove(currentClass);
@@ -105,110 +98,51 @@ function dynamic_dice_faces() {
 function player1() {
 
     dynamic_dice_faces();
-
     // Cas Dé face 1
     if (showClass == "show-1") {
-        ROUND_player1_tmp = 0;
-        GLOBAL_player1 = GLOBAL_player1 - ROUND_player1_tmp;
-        document.getElementById("ROUND_player1").textContent = ROUND_player1_tmp;
-        document.getElementById("GLOBAL_player1").textContent = GLOBAL_player1;
-
-        console.log("ROUND_player1_tmp = " + ROUND_player1_tmp);
-        console.log("ROUND_player1 = " + ROUND_player1);
-
-        //console.clear();
-        console.log("Changement de joueur");
-
         audio_turn_lost.play();
         switchPlayer();
     }
-
     // Cas Dé autres faces
-    else if (showClass != "show-1") {
-        ROUND_player1_tmp = ROUND_player1_tmp + randNum;
-        ROUND_player1 = ROUND_player1 + randNum;
-        console.log("Player 1 / ROUND Score tmp = " + ROUND_player1_tmp);
-        document.getElementById("ROUND_player1").textContent = ROUND_player1_tmp;
+    else {
+        ROUND_player1 = (ROUND_player1 + randNum);
+        document.getElementById("ROUND_player1").textContent = ROUND_player1;
     }
-
-
 }
 
 // JOUEUR 2 / CONDITIONS DU JEU
 function player2() {
 
     dynamic_dice_faces();
-
     // Cas Dé face 1
     if (showClass == "show-1") {
-        ROUND_player2_tmp = 0;
-        GLOBAL_player2 = GLOBAL_player2 - ROUND_player2_tmp;
-        document.getElementById("ROUND_player2").textContent = ROUND_player2_tmp;
-        document.getElementById("GLOBAL_player2").textContent = GLOBAL_player2;
-
-        console.log("ROUND_player2_tmp = " + ROUND_player2_tmp);
-        console.log("ROUND_player2 = " + ROUND_player2);
-
-        //console.clear();
-        console.log("Changement de joueur");
-
         audio_turn_lost.play();
         switchPlayer();
     }
-
     // Cas Dé autres faces
     else {
-        ROUND_player2_tmp = ROUND_player2_tmp + randNum;
-        ROUND_player2 = ROUND_player2 + randNum;
-        console.log("Player 2 / ROUND Score tmp = " + ROUND_player2_tmp);
-        document.getElementById("ROUND_player2").textContent = ROUND_player2_tmp;
+        ROUND_player2 = (ROUND_player2 + randNum);
+        document.getElementById("ROUND_player2").textContent = ROUND_player2;
     }
-
 }
-
-// Lancement des fonctions sur l'évènement click 
-new_game.addEventListener("click", newGame);
-roll_dice.addEventListener("click", player1)
-hold.addEventListener("click", holdResult);
-
-// Bouton enregistrement du Score ROUND dans GLOBAL et permutation du Joueur 
-function holdResult() {
+// Conserver le résultat
+function hold_fx() {
 
     if (current_player == 'p1') {
-
-        // console.clear();
-        console.log("HOLD Score Player 1");
-        GLOBAL_player1 = ROUND_player1;
-        ROUND_player1_tmp = 0;
+        GLOBAL_player1 = GLOBAL_player1 + ROUND_player1;
+        ROUND_player1 = 0;
         document.getElementById("GLOBAL_player1").textContent = GLOBAL_player1;
-        document.getElementById("ROUND_player1").textContent = ROUND_player1_tmp;
-        console.log("Player 1 / GLOBAL Score = " + GLOBAL_player1);
-        console.log("Player 1 / ROUND tmp Score = " + ROUND_player1_tmp);
-        console.log("Player 1 / ROUND Score = " + ROUND_player1);
-
     } else {
-
-        //console.clear();
-        console.log("HOLD Score Player 2");
-        GLOBAL_player2 = ROUND_player2;
-        ROUND_player2_tmp = 0;
+        GLOBAL_player2 = GLOBAL_player2 + ROUND_player2;
+        ROUND_player2 = 0;
+        document.getElementById("ROUND_player2").textContent = ROUND_player2;
         document.getElementById("GLOBAL_player2").textContent = GLOBAL_player2;
-        document.getElementById("ROUND_player2").textContent = ROUND_player2_tmp;
-        console.log("Player 2 / GLOBAL Score = " + GLOBAL_player2);
-        console.log("Player 2 / ROUND tmp Score = " + ROUND_player2_tmp);
-        console.log("Player 2 / ROUND Score = " + ROUND_player2);
     }
-
     switchPlayer();
 }
 
-
-
 // Intervertir les joueurs
 function switchPlayer() {
-
-    ROUND_player1_tmp = 0;
-    ROUND_player2_tmp = 0;
 
     active_player();
 
@@ -217,18 +151,18 @@ function switchPlayer() {
         roll_dice.removeEventListener("click", player1);
         current_player = 'p2';
         game_status = "Player 2's turn";
-        console.log('Player2');
-
+        ROUND_player1 = 0;
+        document.getElementById("ROUND_player1").textContent = ROUND_player1;
     } else {
         roll_dice.addEventListener("click", player1);
         roll_dice.removeEventListener("click", player2);
         current_player = 'p1';
         game_status = "Player 1's turn";
-        console.log('Player1');
+        ROUND_player2 = 0;
+        document.getElementById("ROUND_player2").textContent = ROUND_player2;
     }
     document.getElementById("game_status").textContent = game_status;
     winner();
-
 }
 
 function winner() {
